cb4ed1b558d13f969e38c72405c6e322dbbd9fb3
Ian Griffiths <ian@interact-sw.co.uk>
2016-08-26 07:16:18 +0100

    Add project files.

---
+++ src/Test2/Test2Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    public class Test2Tests
+    {
+        [Test]
+        public void AnotherTest()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
---
+++ src/TestWebApp/Class1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestWebApp
+{
+    public class Class1
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
